6c0c453e18529d110f0fe87915e5644ad1a1655e
Taqien01 <taqien01@gmail.com>
2018-07-29 12:58:14 +0700

    Update Tampilan HelloWorld

---
+++ helloworld.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello World GAE</title>
+</head>
+<body>
+	<?php echo "A,B,C,D" ?>
+	<h1>Ini Buat Apa Ya ??</h1>
+	<img src="https://suryaazuya.com/sites/default/files/animasi_bergerak_0.jpg" width="500" height="350">
+</body>
+</html>
